d9c1466618a0e4e4c7dc43b41bac681d2313d383
jinkook <jinqook0126@naver.com>
2026-04-03 14:38:19 +0900

    feat: 유닛 seed 생성

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,14 @@ import { prisma } from '~/lib/prisma';
 import { units } from './data/units';
 
 async function main() {
+  console.log(process.env.DATABASE_URL);
+
   await prisma.unit.createMany({
     data: units,
     skipDuplicates: true,
   });
 }
-
+console.log('🌱 Seed 시작');
 main()
   .then(() => {
     console.log('🌱 Seed 완료');
