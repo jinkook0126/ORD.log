7b76914f0d1d4213ff8776696cb2527acf411ef2
jinkook <jinqook0126@naver.com>
2026-06-17 17:23:36 +0900

    chore: api/me 주석 삭제

--- app/routes/api/me.ts
+++ app/routes/api/me.ts
@@ -8,25 +8,4 @@ export async function loader({ request }: LoaderFunctionArgs) {
     return new Response(null, { status: 401 });
   }
   return Response.json(user);
-  // const cookie = request.headers.get('cookie') || '';
-
-  // const token = cookie
-  //   .split('; ')
-  //   .find((c) => c.startsWith('accessToken='))
-  //   ?.split('=')[1];
-
-  // if (!token) {
-  //   return new Response(null, { status: 401 });
-  // }
-
-  // try {
-  //   const payload = await verifyAccessToken(token);
-  //   const user = await getUserById(payload.userId);
-  //   if (!user) {
-  //     return new Response(null, { status: 401 });
-  //   }
-  //   return Response.json(user);
-  // } catch {
-  //   return new Response(null, { status: 401 });
-  // }
 }
